2266ec0f3a3ca0cf31ce7f927aecd3d85fa30069
Sophia Lu <sophia.lu@belkin.com>
2019-04-09 19:15:58 -0700

    added content for esp8266andIFTTT

--- examples/ESP8266WithIFTTT/ESP8266WithIFTTT.ino
+++ examples/ESP8266WithIFTTT/ESP8266WithIFTTT.ino
@@ -8,16 +8,25 @@
 #define IFTTT_API_KEY "IFTTT_KEY_GOES_HERE"
 #define IFTTT_EVENT_NAME "IFTTT_EVENT_NAME_HERE"
 
-
 void setup() {
   Serial.begin(115200);
-  while(!Serial) { 
-  }
   Serial.println(" ");// print an empty line before and after Button Press    
   Serial.println("Button Pressed");
   Serial.println(" ");// print an empty line  
 
-  connectToWifi();
+  WiFi.begin(ssid, password);  
+  Serial.println(" ");// print an empty line
+  Serial.print("Attempting to connect: ");
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  } 
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
 
   //just connected to Wi-Fi
   IFTTTWebhook hook(IFTTT_API_KEY, IFTTT_EVENT_NAME);
@@ -32,33 +41,6 @@ void setup() {
   ESP.deepSleep(wakePin); 
 }
 
-
-
-void connectToWifi() {
-  Serial.print("Connecting to: SSID NAME"); //uncomment next line to show SSID name
-  //Serial.print(ssid); 
-  WiFi.begin(ssid, password);  
-  Serial.println(" ");// print an empty line
-  Serial.print("Attempting to connect: ");
-
-  //try to connect for 10 seconds
-  int i = 10;
-  while(WiFi.status() != WL_CONNECTED && i >=0) {
-    delay(1000);
-    Serial.print(i);
-    Serial.print(", ");
-    i--;
-  }
-  Serial.println(" ");// print an empty line
-
-  //print connection result
-  if(WiFi.status() == WL_CONNECTED){
-    Serial.print("Connected."); 
-    Serial.println(" ");// print an empty line
-    Serial.print("NodeMCU ip address: "); 
-    Serial.println(WiFi.localIP());
-  }
-  else {
-    Serial.println("Connection failed - check your credentials or connection");
-  }
+void loop() {
+  Serial.print("This line should not run");
 }
